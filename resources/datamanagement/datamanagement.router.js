bf4cb78e5b17ff6990387cd33f63e487b99755f0
Enrique Meneses <109693762+emnss26@users.noreply.github.com>
2025-06-09 11:55:25 -0600

    Add folder permission utilities and controller

--- resources/datamanagement/datamanagement.router.js
+++ resources/datamanagement/datamanagement.router.js
@@ -5,12 +5,15 @@ const { GetFederatedModel } = require("./datamanagement.controller.js");
 const { GetFoldersStructure } = require("./datamanagement.project.folders.controller.js");
 const { GetFileData } = require("./datamanagement.items.controller.js");
 
+const { GetFolderPermits } = require("./datamanagement.folder.permits.js");
+
 const { GetFileRevisionStatus } = require("./datamanagement.files.reviews.controller.js");
 
 const router = express.Router();
 
 router.get("/items/:accountId/:projectId/federatedmodel", GetFederatedModel);
 router.get("/folders/:accountId/:projectId/folder-structure", GetFoldersStructure);
+router.get("/folders/:accountId/:projectId/permissions", GetFolderPermits);
 
 router.post("/items/:accountId/:projectId/file-data", GetFileData);
 router.post("/items/:accountId/:projectId/file-revisions", GetFileRevisionStatus);
